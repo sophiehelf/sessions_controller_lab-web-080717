57dba48236929bd6711e7a9b342303f30d3f2db2
sophiehelf <sophie.helf@gmail.com>
2017-09-08 11:28:06 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		if params[:name] != nil && params[:name] != ''
+			session[:name] = params[:name]
+			redirect_to '/'
+		else
+			redirect_to '/login'
+		end
+	end
+
+	def destroy 
+		redirect_to '/logout'
+		session.delete :name
+	end
+
+end
